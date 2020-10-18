2fa6c2cb1b89c7763a98c3e8b2a1b07286af8505
fachriansyahmni <fachriansyahmni@gmail.com>
2020-10-18 15:01:59 +0700

    files commit

---
+++ src/pboif2_10119065_latihan9_teks/PBOIF2_10119065_Latihan9_Teks.java
@@ -0,0 +1,21 @@
+/*
+ * @author
+ * NAMA                 : Fachriansyah Muhammad Nur Ihsan
+ * KELAS                : PBOIF2
+ * NIM                  : 10119065
+ * Deskripsi Program    : Program ini menampilkan teks
+ *
+ */
+package pboif2_10119065_latihan9_teks;
+public class PBOIF2_10119065_Latihan9_Teks {
+
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        
+        c = 'A';
+        System.out.println("Hasil"+newline+"Char: "+tab+"berisi "+c);
+    }
+    
+}
